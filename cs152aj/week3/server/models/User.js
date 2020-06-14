40be55e8cec2d15de8c825faca8e14c1b2a0e670
tim <tjhickey@brandeis.edu>
2020-06-14 14:30:30 -0400

    built an authentication demo

---
+++ cs152aj/week3/server/models/User.js
@@ -0,0 +1,22 @@
+
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+
+//var userSchema = mongoose.Schema( {any:{}})
+
+var userSchema = Schema( {
+  googleid: String,
+  googletoken: String,
+  googlename:String,
+  googleemail:String,
+} );
+
+module.exports = mongoose.model( 'User', userSchema );
+
+/*
+newUser.google.id    = profile.id;
+newUser.google.token = token;
+newUser.google.name  = profile.displayName;
+newUser.google.email = profile.emails[0].value; // pull the first email
+*/
