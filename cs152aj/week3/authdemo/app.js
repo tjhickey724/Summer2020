58c9ee0b28d7834875c3d1ef371070e6cfe4544b
tim <tjhickey@brandeis.edu>
2020-06-23 08:51:36 -0400

    added env variables

--- cs152aj/week3/authdemo/app.js
+++ cs152aj/week3/authdemo/app.js
@@ -7,11 +7,12 @@ const cors = require('cors');
 const layouts = require("express-ejs-layouts");
 const auth = require('./config/auth.js');
 
-
+//Created mongolab-amorphous-35976 as MONGODB_URI
 const mongoose = require( 'mongoose' );
 //mongoose.connect( `mongodb+srv://${auth.atlasAuth.username}:${auth.atlasAuth.password}@cluster0-yjamu.mongodb.net/authdemo?retryWrites=true&w=majority`);
 mongoose.connect( 'mongodb://localhost/authDemo');
-
+//const mongoDB_URI = process.env.MONGODB_URI
+//mongoose.connect(mongoDB_URI)
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
